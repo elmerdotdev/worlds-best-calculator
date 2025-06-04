bb7c287c9f50ee778191ac7af6de57b8ee8770ee
Elmer Balbin <contact@elmer.dev>
2025-06-03 20:14:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# World's Best Calculator
+
+This is the world's best calculator. Nothing is better than this.
+
+## Installation
+
+```bash
+npm install @jrbalbin/worlds-best-calculator
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+const add = (a, b, c) => a + b + c
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b
+const divide = (a, b) => a / b
+
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide
+}
